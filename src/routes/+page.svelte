bde344589bbe577c259f98ac2859b6dfc8b7d416
Praktikant <praktikant@Praktikants-MacBook-Pro.local>
2024-01-26 10:16:25 +0100

    Version 5
    Fertig gekürzt aber UI könnte anders

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,20 +1,7 @@
 <script lang="ts">
 let buttons:any = []
 const farbe:string = ''
-let werte:any = {'intern-downloads':'99BCE3',
-                 'intern-logging':'99BCE3',
-                 'intern-automation':'99BCE3',
-                 'intern-share':'B7CA9D',
-                 'intern-namespace':'99BCE3',
-                 'intern-whoami':'99BCE3',
-                 'intern-routing':'99BCE3',
-                 'intern-root':'99BCE3',
-                 'intern-sec':'99BCE3',
-                 'extern-engineering':'FAD791',
-                 'extern-customer':'F2B0A9',
-                 'extern-playground':'99BCE3',
-                 'extern-trial':'FBBF93'
-                }
+let werte:any = {'intern-downloads':'99BCE3', 'intern-logging':'99BCE3', 'intern-automation':'99BCE3','intern-share':'B7CA9D','intern-namespace':'99BCE3','intern-whoami':'99BCE3','intern-routing':'99BCE3','intern-root':'99BCE3','intern-sec':'99BCE3','extern-engineering':'FAD791','extern-customer':'F2B0A9','extern-playground':'99BCE3','extern-trial':'FBBF93'}
 
 function button_click(input:number){
         for(let button of buttons){
@@ -32,13 +19,11 @@ function link(rolle:string,dienstgrad:string,color:string){
 
 <h1>Rollewechsel</h1>
 
-    {#each Object.entries(werte) as [rolle_2,farbe],i }
-        <tr><td><button on:click={() => button_click(i)}>{rolle_2} </button> </td> <td><button bind:this={buttons[i]}  on:click={() => link(rolle_2,'user',farbe)}   class=invisible>user</button></td><td><button bind:this={buttons[i+ 13]}  on:click={() => link(rolle_2,'chef',farbe)}   class=invisible>chef  </button> </td></tr>
-    {/each}
 <div><table>
    <tbody>
-        
-     
+    {#each Object.entries(werte) as [rolle_2,farbe],i }
+    <tr><td><button on:click={() => button_click(i)}>{rolle_2} </button> </td> <td><button bind:this={buttons[i]}  on:click={() => link(rolle_2,'user',farbe)}   class=invisible>user</button></td><td><button bind:this={buttons[i+ 13]}  on:click={() => link(rolle_2,'chef',farbe)}   class=invisible>chef  </button> </td></tr>
+{/each}
    </tbody>
 </table></div>
 
